9a58feeacb1985523f277b8e8ef3e6a790b9d218
Guillermo Navarro <gnavarro0197@gmail.com>
2019-10-23 14:59:08 -0500

    detalles

---
+++ bd/bd_agenciaviaje.sql
@@ -0,0 +1,11 @@
+<div class="error"><h1>Error</h1><p><strong>consulta SQL:</strong>
+<a href="db_sql.php?sql_query=SET+time_zone+%3D+%22%2B00%3A00%22&amp;show_query=1&amp;db=bd_agenciaviaje"><span class="nowrap"><img src="themes/dot.gif" title="Editar" alt="Editar" class="icon ic_b_edit" />&nbsp;Editar</span></a>    </p>
+<p>
+<code class="sql"><pre>
+SET time_zone = &quot;+00:00&quot;
+</pre></code>
+</p>
+<p>
+    <strong>MySQL ha dicho: </strong><a href="./url.php?url=https%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F5.7%2Fen%2Ferror-messages-server.html" target="mysql_doc"><img src="themes/dot.gif" title="Documentación" alt="Documentación" class="icon ic_b_help" /></a>
+</p>
+<code>#1298 - Unknown or incorrect time zone: '+00:00'</code><br/></div>
